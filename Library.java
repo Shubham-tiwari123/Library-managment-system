3f54d023441640c5ba580878cc620e640f638877
Shubham-tiwari123 <st540362@gmail.com>
2018-07-03 09:49:56 +0530

    Library file

--- Library.java
+++ Library.java
@@ -3,18 +3,25 @@ package librarymanagment;
 
 import java.util.Scanner;
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Library extends DbConnection{
+    private final Scanner sc = new Scanner(System.in);
+    
     private int num;
-    private int bookID,issueDate,returnDate;
+    private int bookID;
     private String bookName,studentName;
     private String authorName;
     private String subject;
     private int totalNumber;
     private int count,totalPiece;
     private boolean flag,flag1;
-    private final Scanner sc = new Scanner(System.in);
-    private int names;
-    private int studentId,totalBooks;
+    private int names,num1,num2,num3;
+    private int studentId,totalBooks,totalFine,fineAmount;
+    private DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    LocalDateTime issueDate = null;
+    LocalDateTime returnDate = null;
+    
     public void start(){
         System.out.print("\n1)Store new book\n2)Issue a book\n3)Return a book\n"
                                 + "4)Go Back\n");
@@ -97,21 +104,22 @@ public class Library extends DbConnection{
     }
     
     private void storeBookDb(int bookID,String bookName,String authorName,
-                             String subject,int totalNumber,int choice){
+                             String subject,int totalNumber,int fineamount,int choice){
         try{
             initializeDbConnection();
             try{
                 switch(choice){
                     case 1:
                         System.out.print("\nSaving info..");
-                        st.executeUpdate("INSERT INTO `Books` (BookId,BookName,AuthorName,Subject,TotalNo)"+
-                    "VALUES('"+bookID+"','"+bookName+"','"+authorName+"','"+subject+"','"+totalNumber+"')");
+                        st.executeUpdate("INSERT INTO `Books` (BookId,BookName,AuthorName,Subject,TotalNo,Fine)"+
+                    "VALUES('"+bookID+"','"+bookName+"','"+authorName+"','"+subject+"','"+totalNumber+"','"+fineamount+"')");
                     break;
                     case 2:
                         System.out.print("\nUpdating info..");
                         st.executeUpdate("UPDATE Books SET TotalNo="+totalNumber+" WHERE BookId in ("+bookID+")");
                         break;
                 }
+                start();
             }
             catch(SQLException e){
                 System.out.print("\nError:-"+e);
@@ -257,14 +265,16 @@ public class Library extends DbConnection{
             subject = sc.nextLine();
             System.out.print("Number of books:-");
             totalNumber = sc.nextInt();
-            storeBookDb(bookID, bookName, authorName, subject, totalNumber,1);
+            System.out.print("Fine to be taken:-");
+            fineAmount = sc.nextInt();
+            storeBookDb(bookID, bookName, authorName, subject, totalNumber,fineAmount,1);
         }
         else{
             System.out.print("Number of books:-");
             count = sc.nextInt();
             totalNumber = totalPiece();
             totalNumber = totalNumber+count;
-            storeBookDb(bookID, null, null, null, totalNumber, 2);
+            storeBookDb(bookID, null, null, null, totalNumber,0, 2);
         }
     }
     
@@ -275,15 +285,14 @@ public class Library extends DbConnection{
         flag = checkBook();
         if(flag){
             System.out.print("Invalid Book id...\n");
-            start();
+            issueBook();
         }
         else{
             System.out.print("valid Book id...");
             flag = checkStudentId();
             if(flag){
-                System.out.print("\nIssue date:-");
-                issueDate = sc.nextInt();
-                returnDate = issueDate+15;
+                issueDate = LocalDateTime.now();
+                returnDate = issueDate.plusDays(15);
                 insertBook();
             }
             else{
@@ -295,6 +304,134 @@ public class Library extends DbConnection{
     
     
     public void returnBook(){
-        
+        System.out.print("\nBook id:-");
+        bookID = sc.nextInt();
+        flag = checkBook();
+        if(flag){
+            System.out.print("Invalid Book id...\n");
+            returnBook();
+        }
+        else{
+            System.out.print("valid Book id...");
+            System.out.print("\nStudent id:-");
+            studentId = sc.nextInt();
+            findRecord();
+            checkCredentials();
+        }
+    }
+    
+    private void findRecord(){
+        try{
+            initializeDbConnection();
+            try{
+                rs = st.executeQuery("SELECT * from IssueBook");
+                while(rs.next()){
+                    num1=rs.getInt("BookID");
+                    num2=rs.getInt("LibraryId");
+                    num3=rs.getInt("id");
+                    if(num1==bookID&&num2==studentId)
+                        break;
+                    else
+                        flag=Boolean.TRUE;
+                }
+                if(flag){
+                    System.out.print("Record not found...");
+                    start();
+                }
+            }
+            catch(SQLException e){
+                System.out.print("\nError:-"+e);
+            }
+            finally{
+                closeDbConnection(c);
+            }
+        }
+        catch(SQLException e){
+             System.out.print("\nError:-"+e);
+        }
+        catch(ClassNotFoundException e){
+            System.out.print("\nError:-"+e);
+        }
+    }
+    private void checkCredentials(){
+        try{
+            initializeDbConnection();
+            try{
+                rs = st.executeQuery("SELECT * from StudentRecord WHERE LibraryId = "+studentId);
+                while (rs.next()) {
+                    totalFine = rs.getInt("TatalFine");
+                }
+                if(totalFine==0){
+                    rs = st.executeQuery("SELECT * from Books WHERE BookId = "+bookID);
+                    while (rs.next()) {
+                        totalBooks = rs.getInt("TotalNo");
+                    }
+                    totalBooks = totalBooks+1;
+                    st.executeUpdate("UPDATE Books SET TotalNo="+totalBooks+" WHERE BookId in ("+bookID+")");
+                    
+                    rs = st.executeQuery("SELECT * from StudentRecord WHERE LibraryId = "+studentId);
+                    while (rs.next()) {
+                        totalNumber = rs.getInt("TotalBooks");
+                    }
+                    totalNumber=totalNumber-1;
+                    st.executeUpdate("UPDATE StudentRecord SET TotalBooks="+totalNumber+" WHERE LibraryId in ("+studentId+")");
+                    
+                    st.executeUpdate("DELETE FROM `IssueBook` WHERE id ="+num3);
+                    System.out.print("\nBook returned..");
+                }
+                else{
+                    payFine();
+                }
+            }
+            catch(SQLException e){
+                System.out.print("\nError:-"+e);
+            }
+            finally{
+                closeDbConnection(c);
+            }
+        }
+        catch(SQLException e){
+             System.out.print("\nError:-"+e);
+        }
+        catch(ClassNotFoundException e){
+            System.out.print("\nError:-"+e);
+        }
+    }
+    
+    private void payFine(){
+        System.out.print("\nYou have to pay:-"+totalFine);
+        System.out.print("\n1)Pay\n2)Dont pay:-");
+        num=sc.nextInt();
+        switch(num){
+            case 1:
+                try{
+                    initializeDbConnection();
+                    try{
+                        totalFine=0;
+                        st.executeUpdate("UPDATE StudentRecord SET TatalFine="+totalFine+
+                                " WHERE LibraryId in ("+studentId+")");
+                        checkCredentials();
+                    }
+                    catch(SQLException e){
+                        System.out.print("\nError:-"+e);
+                    }
+                    finally{
+                        closeDbConnection(c);
+                    }
+                }
+                catch(SQLException e){
+                     System.out.print("\nError:-"+e);
+                }
+                catch(ClassNotFoundException e){
+                    System.out.print("\nError:-"+e);
+                }
+                break;
+            case 2:start();
+                   break;
+        }
     }
 }
+
+
+
+
